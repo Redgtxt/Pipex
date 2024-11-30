b7a7a980bea80ca5db602e1c6ec0cce20af804ad
redgtxt <hp052635@gmail.com>
2024-11-30 22:35:29 +0000

    cbum

--- main.c
+++ main.c
@@ -1,107 +1,101 @@
 #include "pipex.h"
 
-void parsing(char *envp[],char *argv[])
-{
-    char **PATH_from_envp;
-    char **mypaths;
-    char **mycmdargs;
-    
-    //Vou apanhar a linha do PATH atraves do meu ENVP
-    PATH_from_envp = ft_substr(envp,0,ft_strlen(envp));
-    mypaths = ft_split(PATH_from_envp,":");
 
-    mycmdargs = ft_split(argv[2]," ");
-
-}
-
-void child_process(int infile,char *cmd1,int p1[2])
-{
-    if(dup2(infile,STDIN_FILENO) == -1)
-    {
-        printf("Erro ao fazer dup do infile\n");
-    }
-    if(dup2(p1[1],STDOUT_FILENO) == -1)
-    {
-        printf("Erro ao fazer dup no Pipe dentro do child process\n");
-    }
-    close(p1[0]);
-    close(infile);
-   // execv(cmd,);//FUNCAO AQUI EXECVE
-    exit(EXIT_FAILURE);//SO VAI RODAR CASO NAO CONSIGA ATIVAR O EXECV
-}
-
-void parent_process(int outfile, char *cmd2,int p1[2])
-{
-    int status;
-
-    waitpid(-1,&status,NULL);
-    if(dup2(outfile,STDOUT_FILENO) == -1)
-    {
-        printf("Erro ao fazer dup no parent process\n");
-    }
-    if(dup2(p1[0],STDIN_FILENO) == -1)
-    {
-        printf("Erro ao fazer dup no Pipe dentro do parent process\n");
-    }
-    close(p1[1]);
-    close(outfile);
-        //execv();//FUNCAO AQUI EXECVE
-    exit(EXIT_FAILURE);//SO VAI RODAR CASO NAO CONSIGA ATIVAR O EXECV
-}
-
-
-void pipex(int infile, int outfile,char *cmd1,char *cmd2)
-{
-    int p1[2];
-    int child;
-    
-    //! Inicializo o Pipe
-    if(pipe(p1) == -1)
-    {
-        printf("Erro no pipe\n");
-    }
-    child = fork();
-    if(child == -1)
-    {
-        printf("Erro ao inicializar o fork\n");
-    }
-
-    if(child == 0)
-        child_process(infile,cmd1,p1);
-    else
-        parent_process(outfile,cmd2,p1);
-}
 int main(int argc,char *argv[],char *envp[])
 {
+
+    //Variaveis
     int infile;
     int outfile;
+    int child;
+    int end[0];
 
-    infile = open(argv[1],O_RDONLY);
-    outfile = open(argv[4],O_CREAT | O_RDWR | O_TRUNC,0644);
-    if(infile < 0 || outfile < 0)
-        return -1;
-    pipex(infile,outfile,argv,envp);
-}
-
-/*
-int main(int argc,char *argv[])
-{
+    /* Erro Handler */
     if(argc != 5)
     {
-        printf("Error: Invalid number of arguments\n");
-        return 1;
+        printf("Number of Arguments not valid\n");
+        return -1;
     }
 
-    char *infile = argv[1];
-    char *cmd1 = argv[2]; //Comando 1
-    char *cmd2 = argv[3]; //Comando 2
-    char *outfile = argv[4];
+    infile = open(argv[1],O_RDONLY);
+    outfile = open(argv[4],O_CREAT | O_RDWR | O_TRUNC, 0644);
 
-    printf("Infile: %s\n",infile);
-    printf("Cmd1: %s\n",cmd1);
-    printf("Cmd2: %s\n",cmd2);
-    printf("Outfile: %s\n",outfile);
 
-    return 0;
+    /* Erro Handler */
+    if(infile < 0 || outfile < 0) 
+        return -1;
+
+    /* Erro Handler */
+    if(pipe(end) == -1)
+    {
+        printf("Error creating the pipe\n");
+        return -1;
+    }
+    
+    child = fork();
+    /* Erro Handler */
+    if(child == -1)
+    {
+        printf("Error creating the fork\n");
+        return -1;
+    }
+
+    /* STEP EVENT */
+
+    /* Vou ter de conseguir as variaveis para puder usar o execve */
+                /*PARSING*/
+
+    char **cmd1;
+    char **cmd2;
+    cmd1 = ft_split(argv[2]," ");
+    cmd2 = ft_split(argv[3]," ");
+
+    if(child == 0)
+    { 
+        //Estou no evento child
+        close(end[0]);
+
+        //Estou a fazer com que o infile fique com o fd de leitura para que possa ser lidas os comandos
+        dup2(infile,STDIN_FILENO);
+        //Ao acabarmos de executar o comando CMD1, vamos passar o output dela para o pip usando o dup2
+        dup2(end[1],STDOUT_FILENO);
+
+        //execve(PATH,ARGS,ENVP);
+        close(end[1]);
+    }else{
+        wait(NULL);
+        //Estou no evento pai
+    }
 }
+
+
+/*
+    execve Arguments
+
+    PATH -- Path do comando: bin/ls
+    ARGS -- Array de argumentos que vao ser executados(tem de terminar no NULL)
+    ENVP -- An array of pointers to null-terminated strings representing the environment variables for the new program. It is also null-terminated. 
+*/
+
+
+/* NOTAS IMPORTANTES
+
+File Descriptor	Constante	Descrição
+0	STDIN_FILENO	Entrada padrão (teclado, por padrão)
+1	STDOUT_FILENO	Saída padrão (tela, por padrão)
+2	STDERR_FILENO	Saída de erro padrão (tela, por padrão)
+
+end[0]: lado de leitura do pipe.
+end[1]: lado de escrita no pipe.
+
+
+O fork() cria dois processos:
+Child (filho): executa cmd1.
+Parent (pai): executa cmd2.
+
+Redireciona a entrada de dados (stdin) para o arquivo infile usando dup2().->   O primeiro comando(cmd 1) precisa receber dados do infile
+                                                                                Dou dup para simular a leitura de um arquivo
+
+ Redireciona a saída de cmd1 para o lado de escrita do pipe (end[1]) usando dup2()-> Tenho de enviar o output do comando cmd1 para o cmd2 atraves de um pipe usando o dup2
+
 */
